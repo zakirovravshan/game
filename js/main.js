db47dba5af14121625d4744a93774fb79480cbc0
zakirovravshan <zakirovrovshan@gmail.com>
2022-11-10 18:20:41 +0500

    add counter

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var elSel = document.querySelector('.game__level')
 var gameInner = document.querySelector('.game__inner')
 var gameList = document.querySelector('.game__list')
 var gameTitle = document.querySelector('.game__title')
+var gamePoint = document.querySelector('.game__point')
 
 
 function random(max) {
@@ -12,7 +13,7 @@ function random(max) {
 function randomTitle(array, node) {
 
     array.forEach((el) => {
-        let item = array[random(100)];
+        var item = array[random(100)];
         node.setAttribute('data-id', item.id);
         node.textContent = item.name;
     });
@@ -27,11 +28,16 @@ function gameRender() {
         var gameImg = document.createElement("img")
         var gameImg = document.createElement("ul")
         var gameImg = document.createElement("img")
+       
+        gamePoint.classList.add("block")
         gameImg.classList.add("game__img")
         randomTitle(array, gameTitle)
         gameImg.src = element.arrayImg
         gameList.appendChild(gameItems)
         gameItems.appendChild(gameImg)
+       
+         
+
     });
 }
 startBtn.addEventListener("click", function () {
@@ -45,11 +51,14 @@ function whiteItem(){
      gameItems.backgroundColor = "#fff"
     
 }
-
+var counter = 0
 gameList.addEventListener("click", function (evt) {
     if (evt.target.matches(".game__item")) {
         if (evt.target.dataset.id == gameTitle.dataset.id) {
-            
+           
+            counter = counter + 1 
+            gamePoint.textContent = counter + "   Point"
+
             evt.target.style.backgroundColor = "#08F543" 
             randomTitle(array, gameTitle)
             setTimeout(() => {
@@ -64,11 +73,27 @@ gameList.addEventListener("click", function (evt) {
         } 
         
         else {
+            
             evt.target.style.backgroundColor = "#F50F39"
+            counter = counter - 1 
+            gamePoint.textContent = counter + "   Point"
+
         }
+
+        
+    }
+    if(counter == -5){
+        gamePoint.classList.add("none")
+        gameList.innerHTML = ""
+       
+        gameTitle.textContent = `GAME OVER YOUR POINT IS    ${counter} `
+       
+        
+
     }
     
 })
+
 
 
 
